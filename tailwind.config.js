2c66f2d8384e96d7ab34b20a7728744147faa823
coderoycc <rcchambi4@umsa.bo>
2023-01-27 16:38:54 -0400

    Mini landing page

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,7 @@
 module.exports = {
   mode: 'jit', //crea los elementos necesarios en la hoja de estilos (motor para seguir los cambios)
   content: ['./*.html'], //indica los elementos que escaneará para aplicar los estilos
+  // darkMode: 'dark', //habilita el modo oscuro
   theme: {
     extend: {},
   },
